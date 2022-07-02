aca3cd7fa26fd030731571cfc3151aa8596c4d55
MrsTrotu <84805996+MrsTrotu@users.noreply.github.com>
2022-07-01 23:48:56 -0500

    created kick.ts

---
+++ commands/moderation/kick.ts
@@ -0,0 +1,26 @@
+import { ICommand } from 'wokcommands'
+
+export default {
+  category: 'Testing',
+  description: 'Replies with pong', // Required for slash commands
+  
+  slash: 'both', // Create both a slash and legacy command
+  testOnly: true, // Only register a slash command for the testing guilds
+  
+  callback: ({ message, interaction }) => {
+    const reply = 'Salte conchetumare'
+
+    // message is provided for a legacy command
+    if (message) {
+      message.reply({
+        content: reply
+      })
+      return
+    }
+
+    // interaction is provided for slash commands
+    interaction.reply({
+      content: reply
+    })
+  },
+} as ICommand
